7f03284b7551a7864d2170afd8871b82544ad494
Amisha <amisha.i@canopas.com>
2022-11-10 12:12:20 +0530

    Initial Commit

---
+++ iLearning/iLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  iLearningApp.swift
+//  iLearning
+//
+//  Created by Amisha Italiya on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
