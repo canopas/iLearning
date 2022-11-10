2489b0549285362f29f01e7058b857e83b100c9e
Amisha <amisha.i@canopas.com>
2022-11-10 12:42:20 +0530

    Added initial project

---
+++ Podfile
@@ -0,0 +1,20 @@
+platform :ios, '15.6'
+
+target 'iLearning' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for iLearning
+    pod 'FirebaseCore'
+    pod 'FirebaseFirestore'
+
+  target 'iLearningTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'iLearningUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ iLearning/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  iLearning
+//
+//  Created by Amisha Italiya on 10/11/22.
+//
+
+import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        let database = Firestore.firestore()
+        return true
+    }
+}
