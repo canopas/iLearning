8691199dbe97ceb0f3f47b5828fac9af04f90295
Amisha <amisha.i@canopas.com>
2022-12-08 12:25:55 +0530

    Added initial login view

--- iLearning/Views/Login/LoginView.swift
+++ iLearning/Views/Login/LoginView.swift
@@ -6,14 +6,60 @@
 //
 
 import SwiftUI
+import UIKit
+import AuthenticationServices
 
 struct LoginView: View {
     var body: some View {
-        VStack {
-            Text("Welcome")
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .center, spacing: 30) {
+                KFAnimatedImage(resource: Bundle.main.url(forResource: "online-education", withExtension: "gif"))
+                    .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.width * 0.8, alignment: .center)
+                
+                Text(R.string.loginScreen.login_screen_text.localized())
+                    .padding(.horizontal, 20)
+                    .multilineTextAlignment(.center)
+                
+                ButtonDividerView()
+                
+                SignInWithAppleButton(.continue) { request in
+                    request.requestedScopes = [.fullName, .email]
+                } onCompletion: { result in
+                    switch result {
+                    case .success(let success):
+                        print("Authorisation successful :: \(success.description)")
+                    case .failure(let error):
+                        print("Authorisation failed: \(error.localizedDescription)")
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width * 0.8, height: 50, alignment: .center)
+
+                Spacer()
+            }
+            .padding(20)
         }
     }
 }
+
+struct ButtonDividerView: View {
+
+    var body: some View {
+        HStack(spacing: 10) {
+            Rectangle()
+                .frame(width: 100, height: 1.0, alignment: .bottom)
+                .foregroundColor(.gray)
+
+            Text(R.string.loginScreen.options_text.localized())
+                .font(.caption)
+                .foregroundColor(.gray)
+
+            Rectangle()
+                .frame(width: 100, height: 1.0, alignment: .bottom)
+                .foregroundColor(.gray)
+        }
+    }
+}
+
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
